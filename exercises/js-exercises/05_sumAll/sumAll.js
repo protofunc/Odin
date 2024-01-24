68cd60f6afae6a73e34d1b05a063c55481c24d99
vedreac <danilo.rodriguez3@gmail.com>
2024-01-24 15:53:31 -0500

    exercise updates

--- exercises/js-exercises/05_sumAll/sumAll.js
+++ exercises/js-exercises/05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
+    // Errors for non-numbers and negative values
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+        return "ERROR";
+    } else if (firstNum < 0 || secondNum < 0) {
+        return "ERROR";
+    }
 
+    // Place larger number last in the array
+    let sumArr = [firstNum, secondNum];
+    if (firstNum > secondNum) {
+        sumArr.shift();
+        sumArr.push(firstNum);
+    }
+    console.log(sumArr);
+    
+    // Add numbers within range specified in parameters
+    let sum = 0;
+    for (let i = 0; i < sumArr[1]; i++) {
+        sum += sumArr[0];
+        sumArr[0]++;
+    }
+    return sum;
 };
+
+console.log(sumAll(0, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
